9c98752bf2de01a562986d0aad4d44f293d223e0
Radosław Więckowski <rwieckowski@ivmx.pl>
2015-04-21 08:50:06 +0200

    Solutions for problems 9 & 10

--- src/main/scala/Euler10.scala
+++ src/main/scala/Euler10.scala
@@ -1,8 +1,14 @@
 /**
-The sum of the primes below 10 is 2  3  5  7  17
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
 
 Find the sum of all the primes below two million
 */
 object Euler10 extends App {
   println("Euler 10")
+  lazy val primes: Stream[Int] = 2 #:: 3 #:: Stream.from(5).filter(n => primes.takeWhile(p => p * p <= n).forall(p => n % p != 0))
+
+  def sumOfPrimesBelow(n: Int) = primes.takeWhile(_ < n).map(BigInt(_)).sum
+
+  println(sumOfPrimesBelow(10))
+  println(sumOfPrimesBelow(2000000))
 }
--- src/main/scala/Euler9.scala
+++ src/main/scala/Euler9.scala
@@ -1,9 +1,20 @@
 /**
-A Pythagorean triplet is a set of three natural numbers <var>a</var> 
-lt <var>b</var> lt <var>c</var> for which
-<div style="text-align:center;"> <var>a</var><sup>2</sup>  <var>b
-</var><sup>2</sup>  <var>c</var><sup>2</sup>
+A Pythagorean triplet is a set of three natural numbers <var>a</var>
+&lt; <var>b</var> &lt; <var>c</var> for which
+<div style="text-align:center;"> <var>a</var><sup>2</sup> + <var>b
+</var><sup>2</sup> = <var>c</var><sup>2</sup>
 */
 object Euler9 extends App {
   println("Euler 9")
+
+  def triplet(n: Int) = for {
+    a <- 1 until n
+    b <- a + 1 until n
+    c <- b + 1 until n
+    if (a * a) + (b * b) == (c * c) && a + b + c == n
+  } yield (a, b, c)
+
+  println(triplet(25))
+  val (a, b, c) = triplet(1000).head
+  println(a * b * c)
 }
